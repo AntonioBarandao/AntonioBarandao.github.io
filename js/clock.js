dc4d5f54bed47ccf659a18fa406a02a791e9e3d6
VoidStorm23 <86311349+VoidStorm23@users.noreply.github.com>
2021-06-22 11:59:45 -0400

    website

---
+++ js/clock.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+
+    function displayTime() {
+        var currentTime = new Date();
+        var hours = currentTime.getHours();
+        var minutes = currentTime.getMinutes();
+        var seconds = currentTime.getSeconds();
+
+
+        var meridiem = "AM";
+        if (hours > 12) {
+            hours = hours - 12;
+            meridiem = "PM";
+        }
+
+        if (hours === 0) {
+            hours = 12;
+        }
+
+
+
+
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+
+
+
+        var clockDiv = document.getElementById('clock');
+
+
+        clockDiv.innerText = hours + ":" + minutes + " " + meridiem;
+    }
+
+
+    displayTime();
+
+    setInterval(displayTime, 1000);
+
+});
